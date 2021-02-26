aecb6da5d4585e3ec95f8c5233e2c185334cc664
Julian ranieri <jumpingafterrain@gmail.com>
2021-02-26 15:00:40 -0800

    lower email

--- flaskblog/users/routes.py
+++ flaskblog/users/routes.py
@@ -14,7 +14,10 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        username = form.username.data
+        email = form.email.data.lower()
+
+        user = User(username=username, email=email, password=hashed_password)
         db.session.add(user)
         db.session.commit()
         flash('Your account has been Created, you are now able to log in.', 'success')
@@ -27,7 +30,8 @@ def login():
         return redirect(url_for('main.home'))
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
+        email = form.email.data.lower()
+        user = User.query.filter_by(email=email).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
