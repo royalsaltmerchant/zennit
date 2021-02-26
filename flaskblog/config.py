74bd0db5e529320b1f47f65d05c8cc975b56ca06
Julian ranieri <jumpingafterrain@gmail.com>
2021-02-26 12:17:55 -0800

    mail situation for now

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -11,5 +11,5 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('EMAIL_USER')
-    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+    MAIL_USERNAME = 'saltdemotester@gmail.com'
+    MAIL_PASSWORD = 'testerdemosalt2'
