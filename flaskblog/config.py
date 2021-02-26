c6848087f0d11e56d4097371943bad758c827e48
Julian ranieri <jumpingafterrain@gmail.com>
2021-02-26 11:18:36 -0800

    sk

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,10 +1,11 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
     #dev
+    # SECRET_KEY = os.environ.get('SECRET_KEY')
     # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     #prod
+    SECRET_KEY = '5c4e04fe660e738d24e38866c63d346a'
     SQLALCHEMY_DATABASE_URI = 'postgres://uanzcelqelrjpl:4bd1cbb543d22bfdda1a9b6b3d65b96940166bc2b007a676bc7369f01ce97f1b@ec2-34-198-31-223.compute-1.amazonaws.com:5432/dftt6bn5j391qb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.googlemail.com'
