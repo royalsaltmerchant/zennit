e00ce6b3c9e570391fa09f39c85eec2acd69c4ff
Julian ranieri <jumpingafterrain@gmail.com>
2021-02-27 10:35:52 -0800

    migrations

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -4,6 +4,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 from flaskblog.config import Config
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -11,11 +12,12 @@ login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 mail = Mail()
+migrate = Migrate()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
-
+    migrate.init_app(app, db)
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
--- flaskblog/models.py
+++ flaskblog/models.py
@@ -12,7 +12,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    image_file = db.Column(db.String(20), nullable=False, default='daruma.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
@@ -41,3 +41,6 @@ class Post(db.Model):
 
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
+
+class Test(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
