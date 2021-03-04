48e6e5570fc477c52000fc80c185b746dd1bbb89
Julian ranieri <jumpingafterrain@gmail.com>
2021-03-03 21:14:06 -0800

    api register working needs serializer

--- frontend/src/components/register.js
+++ frontend/src/components/register.js
@@ -1,86 +1,86 @@
-import React, { Component } from 'react'
-import { Route, Link, Redirect } from "react-router-dom";
+import React, { useState } from 'react';
+import { Route, Link, useHistory } from "react-router-dom";
 import Login from './login'
 import '../main.css'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import axios from 'axios'
 
-export default class Register extends Component {
-  constructor(props) {
-    super(props)
+export default function Register(props) {
+  const [value, setValue] = useState('')
+  const history = useHistory()
 
-    this.state = {
-      value: '',
-      redirect: false
-    }
-
-  }
-
-  setRedirect = () => {
-    this.setState({
-      redirect: true
-    })
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault()
-    console.log(event.target.password.value)
+    history.push("/login")
+    const username = event.target.username.value
+    const email = event.target.email.value
+    const password = event.target.password.value
+
+    try {
+      const res = await axios({
+        method: 'post',
+        url: '/api/register',
+        data: {
+          username: username,
+          email: email,
+          password: password
+        }
+      })
+      console.log(res)
+    } catch (error) {
+      console.log(error)
+    }
+    
   }
+  return (
+    <div>
+      <div className="content-section">
+        <legend className="border-bottom mb-4">Join Zennit!</legend>
+        <Form onSubmit={(event) => handleSubmit(event)}>
+          <Form.Group controlId="username">
+            <Form.Label>Username</Form.Label>
+            <Form.Control required
+              size="lg" 
+              type="username" 
+              placeholder="Username" />
+          </Form.Group>
 
-  render() {
-    return (
-      <div>
-        <div className="content-section">
-          <legend className="border-bottom mb-4">Join Zennit!</legend>
-          <Form onSubmit={(event) => this.handleSubmit(event)}>
-            <Form.Group controlId="username">
-              <Form.Label>Password</Form.Label>
-              <Form.Control required
-                size="lg" 
-                type="username" 
-                placeholder="Username" />
-            </Form.Group>
+          <Form.Group controlId="email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control 
+              required
+              size="lg"
+              type="email"
+              placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
 
-            <Form.Group controlId="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control 
-                required
-                size="lg"
-                type="email"
-                placeholder="Enter email" />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
-
-            <Form.Group controlId="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control 
-                required
-                size="lg" 
-                type="password" 
-                placeholder="Password" 
-                aria-describedby="passwordHelpBlock" />
-              <Form.Text id="passwordHelpBlock" muted>
-                Your password must be 8-20 characters long, contain letters and numbers, and
-                must not contain spaces, special characters, or emoji.
-              </Form.Text>
-            </Form.Group>
-            <Button variant="outline-info" type="submit">
-              Sign Up
-            </Button>
-          </Form>
-        </div>
-        <div className="border-top pt-3">
-          <small className="text-muted">
-            Already Have An Account? <Link className="ml-2" to="/login">Sign In</Link>
-          </small>
-        </div>
+          <Form.Group controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control 
+              required
+              size="lg" 
+              type="password" 
+              placeholder="Password" 
+              aria-describedby="passwordHelpBlock" />
+            <Form.Text id="passwordHelpBlock" muted>
+              Your password must be 8-20 characters long, contain letters and numbers, and
+              must not contain spaces, special characters, or emoji.
+            </Form.Text>
+          </Form.Group>
+          <Button variant="outline-info" type="submit">
+            Sign Up
+          </Button>
+        </Form>
       </div>
-    )
-  }
+      <div className="border-top pt-3">
+        <small className="text-muted">
+          Already Have An Account? <Link className="ml-2" to="/login">Sign In</Link>
+        </small>
+      </div>
+    </div>
+  )
 }
