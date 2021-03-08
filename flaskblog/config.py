7b1327d007c16c75dccc8f84dca902520b922a7c
Julian ranieri <jumpingafterrain@gmail.com>
2021-03-07 18:08:50 -0800

    type env

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASEW_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
