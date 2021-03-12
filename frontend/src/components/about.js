5784927bced685034699602c89b234a16ce2af21
Julian ranieri <jumpingafterrain@gmail.com>
2021-03-12 09:54:43 -0800

    about page write-up

--- frontend/src/components/about.js
+++ frontend/src/components/about.js
@@ -5,7 +5,14 @@ export default class About extends Component {
   render() {
     return (
       <div>
-        <h1>About Page</h1>
+        <h1>About Zennit</h1>
+        <div className="content-section">
+          <h2>A communication platform for today's social zennist!</h2>
+          <p>Zennit was born out of an appreciation for online social discussion regarding Zen. Many Zen-enthusiast online-communities have started in places like: reddit and discord... however, these places did not capture the type of niche online presence that is demanded of such a controversial and perturbing topic. This forum is dedicated to the rigorous discussion of Zen and its related content.</p>
+          <p>To echo our reddit family motto: "The self-nature is originally complete!"</p>
+          <p>Zen, as we understand it here, was born of the Buddhist lineage of Bodhidharma, and has passed through the minds of the Song and Tang dynasty patriarchs, so as to fill our skulls with the enlightened brilliance of the true teaching of the Buddha.</p>
+          <p>This forum is not present as an online educational resource for the history of Zen, but the users here are often willing to share their knowledge with those who seek to understand Zen's dubious past.</p>
+        </div>
       </div>
     )
   }
--- frontend/src/components/sidebar.js
+++ frontend/src/components/sidebar.js
@@ -22,11 +22,14 @@ export default class Sidebar extends Component {
                 <Link to="/about">About</Link>
               </ListGroup.Item>
               <ListGroup.Item>
-                <Link to="/recent_comments">Recent Comments</Link>
+                <Link to="/rules">Rules</Link>
               </ListGroup.Item>
               <ListGroup.Item>
-                <Link to="/users">Users</Link>
+                <Link to="/resources">Resources</Link>
               </ListGroup.Item>
+              {/* <ListGroup.Item>
+                <Link to="/users_list">Users</Link>
+              </ListGroup.Item> */}
               {/* <ListGroup.Item>
                 <Link to="/about">About</Link>
               </ListGroup.Item> */}
