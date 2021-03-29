386591d8590c29333824bb6bb54d146e604df30f
Julian ranieri <jumpingafterrain@gmail.com>
2021-03-28 21:13:32 -0700

    sqlalchemy bug fixed

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,9 +1,12 @@
 import os
+import re
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
