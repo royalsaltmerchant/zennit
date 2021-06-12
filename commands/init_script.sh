0cea3b5bc91984c4e48cc149d0c51157243627bd
Julian ranieri <jumpingafterrain@gmail.com>
2021-06-12 10:42:48 -0700

    init script

---
+++ commands/init_script.sh
@@ -0,0 +1,15 @@
+# install docker
+sudo apt update -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
+sudo apt update -y
+sudo apt install docker-ce -y
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# run zennit
+cd zennit
+sudo docker-compose build
+sudo docker-compose up
